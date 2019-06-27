6644b454c2ba1dc8a02648de90e147a7b72065b3
vuquoctrong <vuquoctrong141@gmail.com>
2019-06-27 11:44:26 +0700

    Initial commit

---
+++ app/src/main/java/com/rikkei/rxjavaticketflighttrainning/network/model/Airline.java
@@ -0,0 +1,19 @@
+package com.rikkei.rxjavaticketflighttrainning.network.model;
+
+public class Airline {
+    int id;
+    String name;
+    String logo;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+}
